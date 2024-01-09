931c74556172b113e22dbc6607345163e4864b2f
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-01-10 00:56:56 +0300

    v1.0

---
+++ Heist Control by lgn.lua
@@ -0,0 +1,28 @@
+--- Heist Control by lgn v1.0 (1.68, 3095) -------------------------"
+
+LGN = '1.0'
+
+--- Гуи ------------------------------------------------------------
+
+require('heistcontrol/system/on_tick.lua')
+require('heistcontrol/util/DrawUI.lua')
+filesys = require('heistcontrol/util/file_system')
+features = require('heistcontrol/rage/features')
+gui = require('heistcontrol/globals/gui')
+
+listener.register('BS_Init', GET_EVENTS_LIST().OnInit, function ()
+    log.init('[Heist Control by lgn] Загрузка скрипта')
+    log.init(string.format('[Heist Control by lgn] Версия -> [%s]', LGN))
+    HOME_SUBMENU:add_separator("Ограбления", "HC_Heists")
+    task.executeAsScript('Init_LoadPages', function ()
+        log.init('[Heist Control by lgn] Загрузка меню')
+        local path = filesys.getInitScriptPath() .. '\\heistcontrol\\pages'
+        path = path:gsub('\\\\lua', '\\lua')
+        for line in io.popen('dir \"' .. path .. '\" /a /b', 'r'):lines() do
+            if filesys.doesFileExist(path .. '\\' .. line .. '\\' .. 'init.lua') then
+                log.init(string.format('[Heist Control by lgn] Инициализация страницы \'%s\'', line))
+                require('heistcontrol/pages/' .. line .. '/' .. 'init')
+            end
+        end
+    end)
+end)
