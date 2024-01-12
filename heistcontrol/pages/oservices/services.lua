5d2f989d1ad5eed16bcbcea8f5b35275113926ff
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-01-12 14:20:30 +0300

    v1.1

---
+++ heistcontrol/pages/oservices/services.lua
@@ -0,0 +1,29 @@
+Services:add_click_option('Вызвать ПКП', 'HC_Stuff_PKP', function ()
+    script_global:new(2738587):at(930):set_int64(1)
+    notify.success('Stuff', 'Вызвано')
+end)
+
+Services:add_click_option('Вызвать Avenger', 'HC_Stuff_Avenger', function ()
+    script_global:new(2738587):at(938):set_int64(1)
+    notify.success('Stuff', 'Вызвано')
+end)
+
+Services:add_click_option('Вызвать TerrorByte', 'HC_Stuff_TerrorByte', function ()
+    script_global:new(2738587):at(943):set_int64(1)
+    notify.success('Stuff', 'Вызвано')
+end)
+
+Services:add_click_option('Вызвать Косатку', 'HC_Stuff_Kosatka', function ()
+    script_global:new(2738587):at(960):set_int64(1)
+    notify.success('Stuff', 'Вызвано')
+end)
+
+Services:add_click_option('Вызвать кислотную лабораторию', 'HC_Stuff_AcidReq', function ()
+    script_global:new(2738587):at(944):set_int64(1)
+    notify.success('Stuff', 'Вызвано')
+end)
+
+Services:add_click_option('Вызвать мотоцикл кислотной лаборатории', 'HC_Stuff_AcidMotoReq', function ()
+    script_global:new(2738587):at(994):set_int64(1)
+    notify.success('Stuff', 'Вызвано')
+end)
