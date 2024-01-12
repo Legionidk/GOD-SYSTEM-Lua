4146d503b72bb4bb87fcd06a372031beacc7cfa5
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-01-12 14:20:19 +0300

    v1.1

---
+++ heistcontrol/pages/oservices/init.lua
@@ -0,0 +1,4 @@
+Services = Submenu.add_static_submenu('Крупный транспорт', 'HC_Services')
+HOME_SUBMENU:add_sub_option('Крупный транспорт', 'HC_Services', Services)
+
+require('heistcontrol/pages/oservices/services.lua')
