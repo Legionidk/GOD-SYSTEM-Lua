b90af77038e29993e317917902c30b56c915ca67
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-01-12 14:21:09 +0300

    v1.1

---
+++ heistcontrol/pages/trash/init.lua
@@ -0,0 +1,8 @@
+Trash = Submenu.add_static_submenu('Мусор', 'HC_Stuff_Trash')
+HOME_SUBMENU:add_sub_option('Мусор', 'HC_Stuff_Trash', Trash):setHint('Юзлесс хуета для прикола.')
+
+Trash:add_click_option('Вызвать Хренча', 'HC_Stuff_Trash_Gooch', function ()
+    script_global:new(2698947):at(3 + 1):set_int64(171)
+    script_global:new(2698947):at(2):set_int64(6)
+    notify.success('Stuff', 'Хренч вызван')
+end)
