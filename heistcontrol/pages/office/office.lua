a5299d85a877c57b5212b3a630cb0c6ddd28a9b6
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-01-16 03:51:53 +0300

    v1.3

--- heistcontrol/pages/office/office.lua
+++ heistcontrol/pages/office/office.lua
@@ -1,9 +1,15 @@
---- Необходимые функции ---------------------------------------------
+--- Луп ящиков -------------------------------------------------
 
-local function stats_set_packed(stat, value, slot)
+Office:add_looped_option('Афк фарм ящиков', 'HC_Office_Afk', 1, function ()
+    player_index = script_global:new(1574915):get_int64()
+    stat = 'MP'..player_index..'_FIXERPSTAT_BOOL1'
     hash = string.smart_joaat(stat)
-    stats.set_packed_stat_bool(hash, value, slot)
-end
+    for i = 12, 16 do
+        stats.set_masked_int(hash, 1, i, player_index)
+    end
+end, function ()
+    notify.success('HC_Office', 'Фарм выключен')
+end):setHint('Каждую секунду тригерит скрипт, который привозит 1-3 ящика во все ваши склады.')
 
 --- Моментальная покупка --------------------------------------------
 
