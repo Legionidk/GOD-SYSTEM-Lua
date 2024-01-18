f89ca493afb246d274cedc435af5477b541ca8b5
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-01-19 02:58:31 +0300

    v1.3.2

--- heistcontrol/pages/ogacidlab/init.lua
+++ heistcontrol/pages/ogacidlab/init.lua
@@ -6,18 +6,18 @@ HOME_SUBMENU:add_sub_option('Кислотная лаборатория', 'HC_Aci
 Acid:add_click_option('Пополнить сырье', 'HC_Acid_InstResupply', function ()
     script_global:new(1662873):at(7):set_int64(1)
     notify.success('Acid', 'Сырье пополнено')
-end):setHint('Бесплатно.')
+end)
 
 --- Кислотка --------------------------------------------------------
 
 Acid:add_click_option('Вызвать кислотную лабораторию', 'HC_Stuff_AcidReq', function ()
     script_global:new(2738587):at(944):set_int64(1)
-    notify.success('Stuff', 'Вызвано')
+    notify.success('Stuff', 'Кислотная лаборатория вызвана')
 end)
 
 --- Мотоцикл кислотки -----------------------------------------------
 
 Acid:add_click_option('Вызвать мотоцикл кислотной лаборатории', 'HC_Stuff_AcidMotoReq', function ()
     script_global:new(2738587):at(994):set_int64(1)
-    notify.success('Stuff', 'Вызвано')
+    notify.success('Stuff', 'Мотоцикл кислотной лаборатории вызван')
 end)
