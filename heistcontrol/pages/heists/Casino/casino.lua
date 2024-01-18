f72619f39535165188f9cbd8c2196505a60ff474
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-01-19 02:50:45 +0300

    v1.3.2

--- heistcontrol/pages/heists/Casino/casino.lua
+++ heistcontrol/pages/heists/Casino/casino.lua
@@ -1,10 +1,3 @@
---- Необходимые функции ---------------------------------------------
-
-local function stats_set(stat, value)
-    hash = string.smart_joaat(stat)
-    stats.set_u32(hash, value)
-end
-
 --- Настройка ограбления --------------------------------------------
 
 Casino = Submenu.add_static_submenu('Казино', 'HC_Casino')
@@ -24,9 +17,8 @@ CasinoSettings:add_choose_option('Подход', 'HC_CasinoSettings_Approach', f
         [2] = 3,
         [3] = 1
     }
-    player_index = script_global:new(1574915):get_int64()
-    stats_set('MP'..player_index..'_H3_LAST_APPROACH', last_approach[pos])
-    stats_set('MP'..player_index..'_H3OPT_APPROACH', pos)
+    stats_set('MP'..local_player()..'_H3_LAST_APPROACH', last_approach[pos])
+    stats_set('MP'..local_player()..'_H3OPT_APPROACH', pos)
     notify.success('Casino', 'Успешно выбрано: ' ..main_position[pos])
 end)
 
@@ -43,8 +35,7 @@ CasinoSettings:add_choose_option('Главная цель', 'HC_CasinoSettings_M
         [3] = 2,
         [4] = 3
     }
-    player_index = script_global:new(1574915):get_int64()
-    stats_set('MP'..player_index..'_H3OPT_TARGET', main_numbers[pos])
+    stats_set('MP'..local_player()..'_H3OPT_TARGET', main_numbers[pos])
     notify.success('Casino', 'Успешно выбрано: ' ..main_position[pos])
 end)
 
@@ -57,11 +48,10 @@ CasinoSettings:add_choose_option('Стрелок', 'HC_CasinoSettings_Gunnman', 
         [5] = 'Патрик Макрири',
         [6] = 'Стрелок удален'
     }
-    player_index = script_global:new(1574915):get_int64()
     if pos == 6 then
-        stats_set('MP'..player_index..'_H3OPT_CREWWEAP', 0)
+        stats_set('MP'..local_player()..'_H3OPT_CREWWEAP', 0)
     else
-        stats_set('MP'..player_index..'_H3OPT_CREWWEAP', pos)
+        stats_set('MP'..local_player()..'_H3OPT_CREWWEAP', pos)
     end
     notify.success('Casino', 'Успешно выбрано: ' ..main_position[pos])
 end)
@@ -75,11 +65,10 @@ CasinoSettings:add_choose_option('Водитель', 'HC_CasinoSettings_Driver',
         [5] = 'Честер Маккой',
         [6] = 'Водитель удален'
     }
-    player_index = script_global:new(1574915):get_int64()
     if pos == 6 then
-        stats_set('MP'..player_index..'_H3OPT_CREWDRIVER', 0)
+        stats_set('MP'..local_player()..'_H3OPT_CREWDRIVER', 0)
     else
-        stats_set('MP'..player_index..'_H3OPT_CREWDRIVER', pos)
+        stats_set('MP'..local_player()..'_H3OPT_CREWDRIVER', pos)
     end
     notify.success('Casino', 'Успешно выбрано: ' ..main_position[pos])
 end)
@@ -93,31 +82,28 @@ CasinoSettings:add_choose_option('Хакер', 'HC_CasinoSettings_Hacker', false
         [5] = 'Пейдж Харрис',
         [6] = 'Хакер удален'
     }
-    player_index = script_global:new(1574915):get_int64()
     if pos == 6 then
-        stats_set('MP'..player_index..'_H3OPT_CREWHACKER', 0)
+        stats_set('MP'..local_player()..'_H3OPT_CREWHACKER', 0)
     else
-        stats_set('MP'..player_index..'_H3OPT_CREWHACKER', pos)
+        stats_set('MP'..local_player()..'_H3OPT_CREWHACKER', pos)
     end
     notify.success('Casino', 'Успешно выбрано: ' ..main_position[pos])
 end)
 
 CasinoSettings:add_click_option('Выполнить подготовительные', 'HC_CasinoSettings_SkipPreps', function ()
-    player_index = script_global:new(1574915):get_int64()
-    stats_set('MP'..player_index..'_H3OPT_DISRUPTSHIP', 3)
-    stats_set('MP'..player_index..'_H3OPT_KEYLEVELS', 2)
-    stats_set('MP'..player_index..'_H3OPT_VEHS', 3)
-    stats_set('MP'..player_index..'_H3OPT_WEAPS', 0)
-    stats_set('MP'..player_index..'_H3OPT_BITSET0', -1)
-    stats_set('MP'..player_index..'_H3OPT_BITSET1', -1)
+    stats_set('MP'..local_player()..'_H3OPT_DISRUPTSHIP', 3)
+    stats_set('MP'..local_player()..'_H3OPT_KEYLEVELS', 2)
+    stats_set('MP'..local_player()..'_H3OPT_VEHS', 3)
+    stats_set('MP'..local_player()..'_H3OPT_WEAPS', 0)
+    stats_set('MP'..local_player()..'_H3OPT_BITSET0', -1)
+    stats_set('MP'..local_player()..'_H3OPT_BITSET1', -1)
     notify.success('Casino', 'Подготовительные выполнены')
 end)
 
 CasinoSettings:add_click_option('Открыть все входы', 'HC_CasinoSettings_OpenPoi', function ()
-    player_index = script_global:new(1574915):get_int64()
-    stats_set('MP'..player_index..'_H3OPT_POI', -1)
-    stats_set('MP'..player_index..'_H3OPT_ACCESSPOINTS', -1)
-    notify.success('Casino', 'Все входы открыты')
+    stats_set('MP'..local_player()..'_H3OPT_POI', -1)
+    stats_set('MP'..local_player()..'_H3OPT_ACCESSPOINTS', -1)
+    notify.success('Casino', 'Входы открыты')
 end)
 
 --- Удалить долю ----------------------------------------------------
@@ -146,22 +132,21 @@ end)
 --- Быстрая настройка ограбления ------------------------------------
 
 Casino:add_click_option('Быстрая настройка ограбления', 'HC_CasinoFast', function ()
-    player_index = script_global:new(1574915):get_int64()
-    stats_set('MP'..player_index..'_H3_LAST_APPROACH', 3) -- Обман
-    stats_set('MP'..player_index..'_H3OPT_APPROACH', 2) -- Обман
-    stats_set('MP'..player_index..'_H3OPT_TARGET', 3) -- Алмазы 
-    stats_set('MP'..player_index..'_H3OPT_CREWWEAP', 1) -- Стрелок Карл Аболаджи (5%)
-    stats_set('MP'..player_index..'_H3OPT_CREWDRIVER', 1) -- Водитель Карим Денц (5%)
-    stats_set('MP'..player_index..'_H3OPT_CREWHACKER', 1) -- Хакер Рикки Люкенс (3%)
-    stats_set('MP'..player_index..'_H3OPT_DISRUPTSHIP', 3) -- Подготовительные
-    stats_set('MP'..player_index..'_H3OPT_KEYLEVELS', 2) -- Подготовительные
-    stats_set('MP'..player_index..'_H3OPT_VEHS', 3) -- Подготовительные
-    stats_set('MP'..player_index..'_H3OPT_WEAPS', 0) -- Подготовительные
-    stats_set('MP'..player_index..'_H3OPT_BITSET0', -1) -- Подготовительные
-    stats_set('MP'..player_index..'_H3OPT_BITSET1', -1) -- Подготовительные
-    stats_set('MP'..player_index..'_H3OPT_POI', -1) -- Входы
-    stats_set('MP'..player_index..'_H3OPT_ACCESSPOINTS', -1) -- Входы
-    notify.success('Casino', 'Ограбление успешно настроено')
+    stats_set('MP'..local_player()..'_H3_LAST_APPROACH', 3) -- Обман
+    stats_set('MP'..local_player()..'_H3OPT_APPROACH', 2) -- Обман
+    stats_set('MP'..local_player()..'_H3OPT_TARGET', 3) -- Алмазы 
+    stats_set('MP'..local_player()..'_H3OPT_CREWWEAP', 1) -- Стрелок Карл Аболаджи (5%)
+    stats_set('MP'..local_player()..'_H3OPT_CREWDRIVER', 1) -- Водитель Карим Денц (5%)
+    stats_set('MP'..local_player()..'_H3OPT_CREWHACKER', 1) -- Хакер Рикки Люкенс (3%)
+    stats_set('MP'..local_player()..'_H3OPT_DISRUPTSHIP', 3) -- Подготовительные
+    stats_set('MP'..local_player()..'_H3OPT_KEYLEVELS', 2) -- Подготовительные
+    stats_set('MP'..local_player()..'_H3OPT_VEHS', 3) -- Подготовительные
+    stats_set('MP'..local_player()..'_H3OPT_WEAPS', 0) -- Подготовительные
+    stats_set('MP'..local_player()..'_H3OPT_BITSET0', -1) -- Подготовительные
+    stats_set('MP'..local_player()..'_H3OPT_BITSET1', -1) -- Подготовительные
+    stats_set('MP'..local_player()..'_H3OPT_POI', -1) -- Входы
+    stats_set('MP'..local_player()..'_H3OPT_ACCESSPOINTS', -1) -- Входы
+    notify.success('Casino', 'Ограбление настроено')
 end):setHint('Автоматически выставляет обман, алмазы и самых низких по доле подельников.')
 
 --- Сброс кд --------------------------------------------------------
@@ -176,21 +161,20 @@ end):setHint('Автоматически выставляет обман, алм
 --- Ресет ограбы ----------------------------------------------------
 
 Casino:add_click_option('Сбросить ограбление', 'HC_CasinoSettings_Reset', function ()
-    player_index = script_global:new(1574915):get_int64()
-    stats_set('MP'..player_index..'_H3_LAST_APPROACH', 0)
-    stats_set('MP'..player_index..'_H3_HARD_APPROACH', 0)
-    stats_set('MP'..player_index..'_H3OPT_APPROACH', 0)
-    stats_set('MP'..player_index..'_H3OPT_TARGET', 0)
-    stats_set('MP'..player_index..'_H3OPT_CREWWEAP', 0)
-    stats_set('MP'..player_index..'_H3OPT_CREWDRIVER', 0)
-    stats_set('MP'..player_index..'_H3OPT_CREWHACKER', 0)
-    stats_set('MP'..player_index..'_H3OPT_DISRUPTSHIP', 0)
-    stats_set('MP'..player_index..'_H3OPT_KEYLEVELS', 0)
-    stats_set('MP'..player_index..'_H3OPT_VEHS', 0)
-    stats_set('MP'..player_index..'_H3OPT_WEAPS', 0)
-    stats_set('MP'..player_index..'_H3OPT_BITSET0', 0)
-    stats_set('MP'..player_index..'_H3OPT_BITSET1', 0)
-    stats_set('MP'..player_index..'_H3OPT_POI', 0)
-    stats_set('MP'..player_index..'_H3OPT_ACCESSPOINTS', 0)
+    stats_set('MP'..local_player()..'_H3_LAST_APPROACH', 0)
+    stats_set('MP'..local_player()..'_H3_HARD_APPROACH', 0)
+    stats_set('MP'..local_player()..'_H3OPT_APPROACH', 0)
+    stats_set('MP'..local_player()..'_H3OPT_TARGET', 0)
+    stats_set('MP'..local_player()..'_H3OPT_CREWWEAP', 0)
+    stats_set('MP'..local_player()..'_H3OPT_CREWDRIVER', 0)
+    stats_set('MP'..local_player()..'_H3OPT_CREWHACKER', 0)
+    stats_set('MP'..local_player()..'_H3OPT_DISRUPTSHIP', 0)
+    stats_set('MP'..local_player()..'_H3OPT_KEYLEVELS', 0)
+    stats_set('MP'..local_player()..'_H3OPT_VEHS', 0)
+    stats_set('MP'..local_player()..'_H3OPT_WEAPS', 0)
+    stats_set('MP'..local_player()..'_H3OPT_BITSET0', 0)
+    stats_set('MP'..local_player()..'_H3OPT_BITSET1', 0)
+    stats_set('MP'..local_player()..'_H3OPT_POI', 0)
+    stats_set('MP'..local_player()..'_H3OPT_ACCESSPOINTS', 0)
     notify.success('Casino', 'Ограбление сброшено')
 end)
