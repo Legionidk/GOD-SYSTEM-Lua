f85d519fc13eac4713fa5acd5cb99a02c35c2bbd
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-01-24 03:56:33 +0300

    v1.4

--- heistcontrol/globals/stuff.lua
+++ heistcontrol/globals/stuff.lua
@@ -13,9 +13,9 @@ Stuff = {
         return true
     end,
     EtaToWaypointData = {
-        text = "",
-        dist = "",
-        arrivalTime = ""
+        text = '',
+        dist = '',
+        arrivalTime = ''
     },
     bodyguardsTarget = nil,
 }
@@ -35,11 +35,45 @@ ParsedFiles = {
 -- }
 
 Patterns = {
-    wepWheelInVehicle = {module = "GTA5.exe", pattern = "44 88 6D E0 EB 06"},
+    wepWheelInVehicle = {module = 'GTA5.exe', pattern = '44 88 6D E0 EB 06'},
 }
 
 Addreses = {
     wepWheelInVehicle = nil,
+}
+
+Apartments = {
+    [0] = {name= 'Офис не найден'},
+    [87]  = {name = 'Западное здание Lombank', coords = {x = -1583.427612,  y =  -557.392517,  z = 33.654240 }},
+    [88]  = {name = 'Западное здание Maze Bank', coords = {x = -1371.461670,   y =  -506.922180, z = 31.857445 }},
+    [89]  = {name = 'Бизнес-центр Arcadius', coords = {x = -106.573792,  y =  -612.973083, z = 34.766911 }},
+    [90]  = {name = 'Башня Maze Bank', coords = {x = -82.427208,   y =  -795.496277,    z = 42.927375}},
+}
+
+Warehouse = {
+    [0]  = {name = 'Склад не найден', capacity = 0,},
+    [1]  = {name = 'Хранилище Пасифик Бейт',      capacity = 16,      coords = {x = 54.191,    y = -2569.248,  z = 6.0046 }}, -- 'MP_WHOUSE_0',
+    [2]  = {name = 'Гараж Уайт Видоу',        capacity = 16,      coords = {x = -1083.054, y = -1261.893,  z = 5.534  }}, -- 'MP_WHOUSE_1',
+    [3]  = {name = 'Блок Целлтау',             capacity = 16,      coords = {x = 896.3665,  y = -1035.749,  z = 35.1096}}, -- 'MP_WHOUSE_2',
+    [4]  = {name = 'Кладовка магазина',  capacity = 16,      coords = {x = 247.473,   y = -1956.943,  z = 23.1908}}, -- 'MP_WHOUSE_3',
+    [5]  = {name = 'Изъятый гараж',         capacity = 16,      coords = {x = -424.828,  y = 185.825,    z = 80.775 }}, -- 'MP_WHOUSE_4',
+    [6]  = {name = 'Газовая фабрика Xero',          capacity = 111,     coords = {x = -1042.482, y = -2023.516,  z = 13.1616}}, -- 'MP_WHOUSE_5',
+    [7]  = {name = 'Derriere на Бэклот', capacity = 42,      coords = {x = -1268.119, y = -812.2741,  z = 17.1075}}, -- 'MP_WHOUSE_6',
+    [8]  = {name = 'Склад Bilgeco',         capacity = 111,     coords = {x = -873.65,   y = -2738.948,  z = 13.9438}}, -- 'MP_WHOUSE_7',
+    [9]  = {name = 'Пирс 400, хозяйственный блок', capacity = 16,      coords = {x = 274.5224,  y = -3015.413,  z = 5.6993 }}, -- 'MP_WHOUSE_8',
+    [10] = {name = 'Склад GEE',             capacity = 42,      coords = {x = 1569.69,   y = -2129.792,  z = 78.3351}}, -- 'MP_WHOUSE_9',
+    [11] = {name = 'Портовый склад №3',      capacity = 42,      coords = {x = -315.551,  y = -2698.654,  z = 7.5495 }}, -- 'MP_WHOUSE_10',
+    [12] = {name = 'Железнодорожный склад',        capacity = 42,      coords = {x = 499.81,    y = -651.982,   z = 24.909 }}, -- 'MP_WHOUSE_11',
+    [13] = {name = 'Пристройка Фриджит',            capacity = 42,      coords = {x = -528.5296, y = -1784.573,  z = 21.5853}}, -- 'MP_WHOUSE_12',
+    [14] = {name = 'Заброшенный фабричный магазин',    capacity = 42,      coords = {x = -295.8596, y = -1353.238,  z = 31.3138}}, -- 'MP_WHOUSE_13',
+    [15] = {name = 'Дисконтная розничная точка',      capacity = 42,      coords = {x = 349.839,   y = 328.889,    z = 104.272}}, -- 'MP_WHOUSE_14',
+    [16] = {name = 'Склад снабжения',           capacity = 111,     coords = {x = 926.2818,  y = -1560.311,  z = 30.7404}}, -- 'MP_WHOUSE_15',
+    [17] = {name = 'Склад Darnell Bros',    capacity = 111,     coords = {x = 759.566,   y = -909.466,   z = 25.244 }}, -- 'MP_WHOUSE_16',
+    [18] = {name = 'Мебельная оптовая точка',       capacity = 111,     coords = {x = 1037.813,  y = -2173.062,  z = 31.5334}}, -- 'MP_WHOUSE_17',
+    [19] = {name = 'Склады Cypress',        capacity = 111,     coords = {x = 1019.116,  y = -2511.69,   z = 28.302 }}, -- 'MP_WHOUSE_18',
+    [20] = {name = 'Западный Вайнвуд, Бэклот',     capacity = 111,     coords = {x = -245.3405, y = 203.3286,   z = 83.818 }}, -- 'MP_WHOUSE_19',
+    [21] = {name = 'Старая электростанция',         capacity = 42,      coords = {x = 539.346,   y = -1945.682,  z = 24.984 }}, -- 'MP_WHOUSE_20',
+    [22] = {name = 'Склад Walker & Sons',   capacity = 111,     coords = {x = 96.1538,   y = -2216.4,    z = 6.1712 }}, -- 'MP_WHOUSE_21',
 }
 
 GetSelectedPlayer = function () end
@@ -49,8 +83,8 @@ function mp()
 end
 
 draw.crop_text = function (text_s, len_n)
-    if len_n <= 0 then return "" end
-    if string.len(text_s) == 0 then return "" end
+    if len_n <= 0 then return '' end
+    if string.len(text_s) == 0 then return '' end
     if draw.get_text_size(text_s).x <= len_n then return text_s end
     return draw.crop_text(text_s:sub(1, -2), len_n)
 end
