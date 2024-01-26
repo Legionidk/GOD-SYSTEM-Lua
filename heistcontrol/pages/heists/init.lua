4927a241d074ab930452c255559e2a2c28848725
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-01-26 14:31:10 +0300

    v1.6

--- heistcontrol/pages/heists/init.lua
+++ heistcontrol/pages/heists/init.lua
@@ -1,7 +1,8 @@
 Heists = Submenu.add_static_submenu('Ограбления', 'HC_Heists')
 HOME_SUBMENU:add_sub_option('Ограбления', 'HC_Heists', Heists)
 
-require('heistcontrol/pages/heists/Dr. Dre/dre.lua')
 require('heistcontrol/pages/heists/Cayo/cayo.lua')
 require('heistcontrol/pages/heists/Casino/casino.lua')
 require('heistcontrol/pages/heists/Auto Shop/auto.lua')
+require('heistcontrol/pages/heists/Salvage/salvage.lua')
+require('heistcontrol/pages/heists/Dr. Dre/dre.lua')
