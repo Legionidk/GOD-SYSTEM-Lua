71d6c2c9cf2aef176d4b6b6c4412b606be0317b1
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-01-26 14:29:04 +0300

    v1.6

--- heistcontrol/pages/heists/Cayo/cayo.lua
+++ heistcontrol/pages/heists/Cayo/cayo.lua
@@ -8,118 +8,75 @@ Cayo:add_sub_option('Настройка ограбления', 'HC_CayoSettings'
 
 CayoSettings:add_choose_option('Сложность', 'HC_CayoSettings_Difficulty', false, {'Высокая', 'Нормальная'},
 function (pos)
-    main_position = {
-        [1] = 'Высокая сложность',
-        [2] = 'Нормальная сложность'
-    }
-    main_numbers = {
-        [1] = 131055,
-        [2] = 126823
-    }
-    stats_set('MP'..mp()..'_H4_PROGRESS', main_numbers[pos])
-    notify.success('Cayo', 'Успешно выбрано: ' ..main_position[pos])
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4_PROGRESS'), CayoDiff_val[pos])
+    notify.success('Cayo', 'Успешно выбрано: ' ..CayoDiff_name[pos])
 end)
 
 CayoSettings:add_choose_option('Главная цель', 'HC_CayoSettings_Main', false, {'Статуя пантеры', 'Розовый алмаз', 'Облигации', 'Ожерелье', 'Текила "Синсимито"'},
 function (pos)
-    main_position = {
-        [1] = 'Статуя пантеры',
-        [2] = 'Розовый алмаз',
-        [3] = 'Облигации',
-        [4] = 'Ожерелье',
-        [5] = 'Текила "Синсимито"'
-    }
-    main_numbers = {
-        [1] = 5,
-        [2] = 3,
-        [3] = 2,
-        [4] = 1,
-        [5] = 0
-    }
-    stats_set('MP'..mp()..'_H4CNF_TARGET', main_numbers[pos])
-    notify.success('Cayo', 'Успешно выбрано: ' ..main_position[pos])
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_TARGET'), CayoMain_val[pos])
+    notify.success('Cayo', 'Успешно выбрано: ' ..CayoMain_name[pos])
 end)
 
 CayoSettings:add_choose_option('Второстепенная цель', 'HC_CayoSettings_Second', false, {'Золото', 'Картины', 'Кокаин', 'Марихуана', 'Деньги'},
 function (pos)
-    main_position = {
-        [1] = 'Золото',
-        [2] = 'Картины',
-        [3] = 'Кокаин',
-        [4] = 'Марихуана',
-        [5] = 'Деньги'
-    }
     if pos == 1 then
         clear('PAINT', 'COKE', 'WEED', 'CASH')
-        stats_set('MP'..mp()..'_H4LOOT_GOLD_C', 255)
-        stats_set('MP'..mp()..'_H4LOOT_GOLD_C_SCOPED', 255)
-        stats_set('MP'..mp()..'_H4LOOT_GOLD_I', -1)
-        stats_set('MP'..mp()..'_H4LOOT_GOLD_I_SCOPED', -1)
-        stats_set('MP'..mp()..'_H4LOOT_GOLD_V', 330350)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_GOLD_C'), 255)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_GOLD_C_SCOPED'), 255)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_GOLD_I'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_GOLD_I_SCOPED'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_GOLD_V'), 330350)
     elseif pos == 2 then
         clear('GOLD', 'COKE', 'WEED', 'CASH')
-        stats_set('MP'..mp()..'_H4LOOT_PAINT', -1)
-        stats_set('MP'..mp()..'_H4LOOT_PAINT_SCOPED', -1)
-        stats_set('MP'..mp()..'_H4LOOT_PAINT_V', 403500)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_PAINT'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_PAINT_SCOPED'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_PAINT_V'), 403500)
     elseif pos == 3 then
         clear('PAINT', 'GOLD', 'WEED', 'CASH')
-        stats_set('MP'..mp()..'_H4LOOT_COKE_C', -1)
-        stats_set('MP'..mp()..'_H4LOOT_COKE_C_SCOPED', -1)
-        stats_set('MP'..mp()..'_H4LOOT_COKE_I', -1)
-        stats_set('MP'..mp()..'_H4LOOT_COKE_I_SCOPED', -1)
-        stats_set('MP'..mp()..'_H4LOOT_COKE_V', 330350)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_COKE_C'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_COKE_C_SCOPED'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_COKE_I'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_COKE_I_SCOPED'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_COKE_V'), 330350)
     elseif pos == 4 then
         clear( 'PAINT', 'GOLD', 'COKE', 'CASH')
-        stats_set('MP'..mp()..'_H4LOOT_WEED_C', -1)
-        stats_set('MP'..mp()..'_H4LOOT_WEED_C_SCOPED', -1)
-        stats_set('MP'..mp()..'_H4LOOT_WEED_I', -1)
-        stats_set('MP'..mp()..'_H4LOOT_WEED_I_SCOPED', -1)
-        stats_set('MP'..mp()..'_H4LOOT_WEED_V', 330350)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_WEED_C'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_WEED_C_SCOPED'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_WEED_I'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_WEED_I_SCOPED'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_WEED_V'), 330350)
     elseif pos == 5 then
         clear( 'PAINT', 'GOLD', 'COKE', 'WEED')
-        stats_set('MP'..mp()..'_H4LOOT_CASH_C', -1)
-        stats_set('MP'..mp()..'_H4LOOT_CASH_C_SCOPED', -1)
-        stats_set('MP'..mp()..'_H4LOOT_CASH_I', -1)
-        stats_set('MP'..mp()..'_H4LOOT_CASH_I_SCOPED', -1)
-        stats_set('MP'..mp()..'_H4LOOT_CASH_V', 330350)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_CASH_C'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_CASH_C_SCOPED'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_CASH_I'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_CASH_I_SCOPED'), -1)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_CASH_V'), 330350)
     end
-    notify.success('Cayo', 'Успешно выбрано: ' ..main_position[pos])
+    notify.success('Cayo', 'Успешно выбрано: ' ..CayoSec_name[pos])
 end)
 
 CayoSettings:add_choose_option('Оружие', 'HC_CayoSettings_Guns', false, {'Зачинщик', 'Конспиратор', 'Снайпер', 'Диверсант', 'Марксман'},
 function (pos)
-    main_position = {
-        [1] = 'Зачинщик',
-        [2] = 'Конспиратор',
-        [3] = 'Снайпер',
-        [4] = 'Диверсант',
-        [5] = 'Марксман'
-    }
-    main_numbers = {
-        [1] = 1,
-        [2] = 2,
-        [3] = 3,
-        [4] = 4,
-        [5] = 5
-    }
-    stats_set('MP'..mp()..'_H4CNF_WEAPONS', main_numbers[pos])
-    notify.success('Cayo', 'Успешно выбрано: ' ..main_position[pos])
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_WEAPONS'), CayoGuns_val[pos])
+    notify.success('Cayo', 'Успешно выбрано: ' ..CayoGuns_name[pos])
 end)
 
 CayoSettings:add_click_option('Выполнить подготовительные', 'HC_CayoSettings_Preps', function()
-    stats_set('MP'..mp()..'_H4CNF_UNIFORM', -1) -- Подготовительные
-    stats_set('MP'..mp()..'_H4CNF_TROJAN', 5) -- Подготовительные
-    stats_set('MP'..mp()..'_H4CNF_ARM_DISRP', 3) -- Подготовительные
-    stats_set('MP'..mp()..'_H4CNF_WEP_DISRP', 3) -- Подготовительные
-    stats_set('MP'..mp()..'_H4CNF_HEL_DISRP', 3) -- Подготовительные
-    stats_set('MP'..mp()..'_H4CNF_GRAPPEL', -1) -- Подготовительные
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_UNIFORM'), -1)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_TROJAN'), 5)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_ARM_DISRP'), 3)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_WEP_DISRP'), 3)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_HEL_DISRP'), 3)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_GRAPPEL'), -1)
     notify.success('Cayo', 'Подготовительные выполнены')
 end)
 
 CayoSettings:add_click_option('Открыть все выходы', 'HC_CayoSettings_Exits', function()
-    stats_set('MP'..mp()..'_H4CNF_BS_GEN', -1) -- Входы походу хуй знает
-    stats_set('MP'..mp()..'_H4CNF_BS_ENTR', 63) -- Входы походу хуй знает
-    stats_set('MP'..mp()..'_H4CNF_APPROACH', -1) -- Входы походу хуй знает
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_BS_GEN'), -1)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_BS_ENTR'), 63)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_APPROACH'), -1)
     notify.success('Cayo', 'Выходы открыты')
 end)
 
@@ -127,32 +84,32 @@ end)
 
 Cayo:add_click_option('Быстрая настройка ограбления', 'HC_CayoFast', function()
     clear('PAINT', 'COKE', 'WEED', 'CASH')
-    stats_set('MP'..mp()..'_H4_PROGRESS', 131055) -- Сложность (131055 - хард)
-    stats_set('MP'..mp()..'_H4CNF_TARGET', 5) -- Главная цель (5 - пантера)
-    stats_set('MP'..mp()..'_H4LOOT_GOLD_C', 255) -- Спавн золота
-    stats_set('MP'..mp()..'_H4LOOT_GOLD_C_SCOPED', 255) -- Спавн золота
-    stats_set('MP'..mp()..'_H4LOOT_GOLD_I', -1) -- Спавн золота (остров)
-    stats_set('MP'..mp()..'_H4LOOT_GOLD_I_SCOPED', -1) -- Спавн золота (остров)
-    stats_set('MP'..mp()..'_H4LOOT_GOLD_V', 330350) -- Множитель цены золота (330350 - подходит под пантеру и фулл сумку золота)
-    stats_set('MP'..mp()..'_H4_MISSIONS', 65535) -- Транспорт (65536 - весь транспорт)
-    stats_set('MP'..mp()..'_H4CNF_WEAPONS', 2) -- Оружие (2 - конспиратор)
-    stats_set('MP'..mp()..'_H4CNF_UNIFORM', -1) -- Подготовительные
-    stats_set('MP'..mp()..'_H4CNF_TROJAN', 5) -- Подготовительные
-    stats_set('MP'..mp()..'_H4CNF_ARM_DISRP', 3) -- Подготовительные
-    stats_set('MP'..mp()..'_H4CNF_WEP_DISRP', 3) -- Подготовительные
-    stats_set('MP'..mp()..'_H4CNF_HEL_DISRP', 3) -- Подготовительные
-    stats_set('MP'..mp()..'_H4CNF_GRAPPEL', -1) -- Подготовительные
-    stats_set('MP'..mp()..'_H4CNF_BS_GEN', -1) -- Входы походу хуй знает
-    stats_set('MP'..mp()..'_H4CNF_BS_ENTR', 63) -- Входы походу хуй знает
-    stats_set('MP'..mp()..'_H4CNF_APPROACH', -1) -- Входы походу хуй знает
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4_PROGRESS'), 131055)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_TARGET'), 5)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_GOLD_C'), 255)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_GOLD_C_SCOPED'), 255)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_GOLD_I'), -1)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_GOLD_I_SCOPED'), -1)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4LOOT_GOLD_V'), 330350)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4_MISSIONS'), 65535)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_WEAPONS'), 2)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_UNIFORM'), -1)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_TROJAN'), 5)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_ARM_DISRP'), 3)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_WEP_DISRP'), 3)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_HEL_DISRP'), 3)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_GRAPPEL'), -1)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_BS_GEN'), -1)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_BS_ENTR'), 63)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4CNF_APPROACH'), -1)
     notify.success('Cayo', 'Ограбление настроено')
 end):setHint('Автоматически выставляет высокий уровень сложности, статую пантеры и золото.')
 
 --- Удалить долю ----------------------------------------------------
 
 Cayo:add_click_option('Удалить долю Павла и команды', 'HC_CayoCut', function()
-    script_global:new(262145):at(30268):set_float(0)
-    script_global:new(262145):at(30269):set_float(0)
+    script_global:new(262145):at(30268):set_int64(0)
+    script_global:new(262145):at(30269):set_int64(0)
     notify.success('Cayo', 'Доли удалены')
 end):setHint('Активировать после начала ограбления.')
 
@@ -172,11 +129,11 @@ end)
 
 Cayo:add_choose_option('Сбросить кд ограбления', 'HC_CayoCd', false, {'Соло', 'Мульти'}, function(pos)
     if pos == 1 then
-        stats_set('MP'..mp()..'_H4_TARGET_POSIX', 1659643454)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4_TARGET_POSIX'), 1659643454)
     else
-        stats_set('MP'..mp()..'_H4_TARGET_POSIX', 1659429119)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_H4_TARGET_POSIX'), 1659429119)
     end
-    stats_set('MP'..mp()..'_H4_COOLDOWN', 0)
-    stats_set('MP'..mp()..'_H4_COOLDOWN_HARD', 0)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4_COOLDOWN'), 0)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_H4_COOLDOWN_HARD'), 0)
     notify.success('Cayo', 'Кд сброшено, перезайдите в онлайн')
 end):setHint('После активации необходимо перезайти в онлайн.')
