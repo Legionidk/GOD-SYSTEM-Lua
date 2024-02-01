2bde40e93e33df75acce735bb652c76a759efc33
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-02-01 18:22:06 +0300

    v1.8

--- heistcontrol/pages/heists/heists.lua
+++ heistcontrol/pages/heists/heists.lua
@@ -3,6 +3,7 @@ HOME_SUBMENU:add_sub_option('Ограбления', 'HC_Heists', Heists)
 
 require('heistcontrol/pages/heists/Cayo/cayo.lua')
 require('heistcontrol/pages/heists/Casino/casino.lua')
+require('heistcontrol/pages/heists/Doomsday/doomsday.lua')
 require('heistcontrol/pages/heists/Auto Shop/auto.lua')
 require('heistcontrol/pages/heists/Salvage/salvage.lua')
 require('heistcontrol/pages/heists/Dr. Dre/dre.lua')
