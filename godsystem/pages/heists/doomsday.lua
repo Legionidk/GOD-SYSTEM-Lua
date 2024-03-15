76143443f44da1d8b40d24b68da3e51a573110e8
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-03-15 23:48:44 +0300

    v2.3

---
+++ godsystem/pages/heists/doomsday.lua
@@ -0,0 +1,37 @@
+--- Акты ------------------------------------------------------------
+
+Doomsday = Submenu.add_static_submenu('Судный день', 'HC_DD')
+Heists:add_sub_option('Судный день', 'HC_Heists_DD', Doomsday)
+
+Doomsday:add_choose_option('Выбрать акт', 'HC_DD_Act', false, DDAct_name, function (pos)
+    if pos == 1 then
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_FLOW_MISSION_PROG'), 503)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_HEIST_STATUS'), 229383)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_FLOW_NOTIFICATIONS'), 1557)
+        notify.success('HC_DD', 'Успешно выбрано: '..DDAct_name[pos])
+    elseif pos == 2 then
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_FLOW_MISSION_PROG'), 240)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_HEIST_STATUS'), 229378)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_FLOW_NOTIFICATIONS'), 1557)
+        notify.success('HC_DD', 'Успешно выбрано: '..DDAct_name[pos])
+    else
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_FLOW_MISSION_PROG'), 16368)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_HEIST_STATUS'), 229380)
+        stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_FLOW_NOTIFICATIONS'), 1557)
+        notify.success('HC_DD', 'Успешно выбрано: '..DDAct_name[pos])
+    end
+end)
+
+--- Препы -----------------------------------------------------------
+
+Doomsday:add_click_option('Выполнить подготовительные', 'HC_DD_Preps', function ()
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_FM_MISSION_PROG'), -1)
+    notify.success('HC_DD', 'Подготовительные выполнены')
+end)
+
+Doomsday:add_click_option('Сбросить сценарий', 'HC_DD_Preps', function ()
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_FLOW_MISSION_PROG'), 240)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_HEIST_STATUS'), 0)
+    stats.set_u32(string.smart_joaat('MP'..mp()..'_GANGOPS_FLOW_NOTIFICATIONS'), 1557)
+    notify.success('HC_DD', 'Сценарий сброшен')
+end)
---
+++ godsystem/pages/heists/heists.lua
@@ -0,0 +1,9 @@
+Heists = Submenu.add_static_submenu('Ограбления', 'HC_Heists')
+HOME_SUBMENU:add_sub_option('Ограбления', 'HC_Heists', Heists)
+
+require('godsystem/pages/heists/cayo.lua')
+require('godsystem/pages/heists/casino.lua')
+require('godsystem/pages/heists/doomsday.lua')
+require('godsystem/pages/heists/auto.lua')
+require('godsystem/pages/heists/salvage.lua')
+require('godsystem/pages/heists/dre.lua')
