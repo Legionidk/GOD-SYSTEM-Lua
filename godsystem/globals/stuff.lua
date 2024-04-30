a74b172d39113f1d02ce5e8e04531c316332d79a
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-04-30 13:29:53 +0300

    v2.6

--- godsystem/globals/stuff.lua
+++ godsystem/globals/stuff.lua
@@ -42,8 +42,8 @@ function addon_checker()
             end
         end
     end
-    log.success('GOD SYSTEM', 'Подгружено аддонов: '..#Checked_addons)
-    notify.success('GOD SYSTEM', 'Подгружено аддонов: '..#Checked_addons)
+    log.success('GOD SYSTEM', 'Loaded mods: '..#Checked_addons)
+    notify.success('GOD SYSTEM', 'Loaded mods: '..#Checked_addons)
 end
 
 --- Разные данные ---------------------------------------------------
@@ -262,48 +262,48 @@ Removed_veh = {
     [197] = 35678}
 
 Apartments = {
-    [0] = {name= 'Офис не найден'},
-    [87]  = {name = 'Западное здание Lombank',      coords = {x = -1583.427612,  y =  -557.392517,  z = 33.654240 }},
-    [88]  = {name = 'Западное здание Maze Bank',    coords = {x = -1371.461670,   y =  -506.922180, z = 31.857445 }},
-    [89]  = {name = 'Бизнес-центр Arcadius',        coords = {x = -106.573792,  y =  -612.973083, z = 34.766911 }},
-    [90]  = {name = 'Башня Maze Bank',              coords = {x = -82.427208,   y =  -795.496277,    z = 42.927375}}
+    [0] = {name= 'Office not found'},
+    [87]  = {name = 'Lombank West',                    coords = {x = -1583.427612,  y =  -557.392517,  z = 33.654240 }},
+    [88]  = {name = 'Maze Bank West',                  coords = {x = -1371.461670,   y =  -506.922180, z = 31.857445 }},
+    [89]  = {name = 'Arcadius Business Center',        coords = {x = -106.573792,  y =  -612.973083, z = 34.766911 }},
+    [90]  = {name = 'Maze Bank Tower',                 coords = {x = -82.427208,   y =  -795.496277,    z = 42.927375}}
 }
 
 Warehouse = {
-    [0]  = {name = 'Склад не найден',               capacity = 0,},
-    [1]  = {name = 'Хранилище Пасифик Бейт',        capacity = 16,      coords = {x = 54.191,    y = -2569.248,  z = 6.0046 }},
-    [2]  = {name = 'Гараж Уайт Видоу',              capacity = 16,      coords = {x = -1083.054, y = -1261.893,  z = 5.534  }},
-    [3]  = {name = 'Блок Целлтау',                  capacity = 16,      coords = {x = 896.3665,  y = -1035.749,  z = 35.1096}},
-    [4]  = {name = 'Кладовка магазина',             capacity = 16,      coords = {x = 247.473,   y = -1956.943,  z = 23.1908}},
-    [5]  = {name = 'Изъятый гараж',                 capacity = 16,      coords = {x = -424.828,  y = 185.825,    z = 80.775 }},
-    [6]  = {name = 'Газовая фабрика Xero',          capacity = 111,     coords = {x = -1042.482, y = -2023.516,  z = 13.1616}},
-    [7]  = {name = 'Derriere на Бэклот',            capacity = 42,      coords = {x = -1268.119, y = -812.2741,  z = 17.1075}},
-    [8]  = {name = 'Склад Bilgeco',                 capacity = 111,     coords = {x = -874,   y = -2736,  z = 13.9438}},
-    [9]  = {name = 'Пирс 400, хозяйственный блок',  capacity = 16,      coords = {x = 274.5224,  y = -3015.413,  z = 5.6993 }},
-    [10] = {name = 'Склад GEE',                     capacity = 42,      coords = {x = 1569.69,   y = -2129.792,  z = 78.3351}},
-    [11] = {name = 'Портовый склад №3',             capacity = 42,      coords = {x = -315.551,  y = -2698.654,  z = 7.5495 }},
-    [12] = {name = 'Железнодорожный склад',         capacity = 42,      coords = {x = 499.81,    y = -651.982,   z = 24.909 }},
-    [13] = {name = 'Пристройка Фриджит',            capacity = 42,      coords = {x = -528.5296, y = -1784.573,  z = 21.5853}},
-    [14] = {name = 'Заброшенный фабричный магазин', capacity = 42,      coords = {x = -295.8596, y = -1353.238,  z = 31.3138}},
-    [15] = {name = 'Дисконтная розничная точка',    capacity = 42,      coords = {x = 349.839,   y = 328.889,    z = 104.272}},
-    [16] = {name = 'Склад снабжения',               capacity = 111,     coords = {x = 926.2818,  y = -1560.311,  z = 30.7404}},
-    [17] = {name = 'Склад Darnell Bros',            capacity = 111,     coords = {x = 759.566,   y = -909.466,   z = 25.244 }},
-    [18] = {name = 'Мебельная оптовая точка',       capacity = 111,     coords = {x = 1037.813,  y = -2173.062,  z = 31.5334}},
-    [19] = {name = 'Склады Сайпресс',               capacity = 111,     coords = {x = 1019.116,  y = -2511.69,   z = 28.302 }},
-    [20] = {name = 'Западный Вайнвуд, Бэклот',      capacity = 111,     coords = {x = -245.3405, y = 203.3286,   z = 83.818 }},
-    [21] = {name = 'Старая электростанция',         capacity = 42,      coords = {x = 539.346,   y = -1945.682,  z = 24.984 }},
-    [22] = {name = 'Склад Walker & Sons',           capacity = 111,     coords = {x = 96.1538,   y = -2216.4,    z = 6.1712 }}
+    [0]  = {name = 'Warehouse not found',           capacity = 0,},
+    [1]  = {name = 'Pacific Bait Storage',          capacity = 16,      coords = {x = 54.191,    y = -2569.248,  z = 6.0046 }},
+    [2]  = {name = 'White Widow Garage',            capacity = 16,      coords = {x = -1083.054, y = -1261.893,  z = 5.534  }},
+    [3]  = {name = 'Celltowa Unit',                 capacity = 16,      coords = {x = 896.3665,  y = -1035.749,  z = 35.1096}},
+    [4]  = {name = 'Convenience Store Lockup',      capacity = 16,      coords = {x = 247.473,   y = -1956.943,  z = 23.1908}},
+    [5]  = {name = 'Foreclosed Garage',             capacity = 16,      coords = {x = -424.828,  y = 185.825,    z = 80.775 }},
+    [6]  = {name = 'Xero Gas Factory',              capacity = 111,     coords = {x = -1042.482, y = -2023.516,  z = 13.1616}},
+    [7]  = {name = 'Derriere Lingerie Backlot',     capacity = 42,      coords = {x = -1268.119, y = -812.2741,  z = 17.1075}},
+    [8]  = {name = 'Bilgeco Warehouse',             capacity = 111,     coords = {x = -874,   y = -2736,  z = 13.9438}},
+    [9]  = {name = 'Pier 400 Utility Building',     capacity = 16,      coords = {x = 274.5224,  y = -3015.413,  z = 5.6993 }},
+    [10] = {name = 'GEE Warehouse',                 capacity = 42,      coords = {x = 1569.69,   y = -2129.792,  z = 78.3351}},
+    [11] = {name = 'LS Marine Building 3',          capacity = 42,      coords = {x = -315.551,  y = -2698.654,  z = 7.5495 }},
+    [12] = {name = 'Railyard Warehouse',            capacity = 42,      coords = {x = 499.81,    y = -651.982,   z = 24.909 }},
+    [13] = {name = 'Fridgit Annexe',                capacity = 42,      coords = {x = -528.5296, y = -1784.573,  z = 21.5853}},
+    [14] = {name = 'Disused Factory Outlet',        capacity = 42,      coords = {x = -295.8596, y = -1353.238,  z = 31.3138}},
+    [15] = {name = 'Discount Retail Unit',          capacity = 42,      coords = {x = 349.839,   y = 328.889,    z = 104.272}},
+    [16] = {name = 'Logistics Depot',               capacity = 111,     coords = {x = 926.2818,  y = -1560.311,  z = 30.7404}},
+    [17] = {name = 'Darnell Bros Warehouse',        capacity = 111,     coords = {x = 759.566,   y = -909.466,   z = 25.244 }},
+    [18] = {name = 'Wholesale Furniture',           capacity = 111,     coords = {x = 1037.813,  y = -2173.062,  z = 31.5334}},
+    [19] = {name = 'Cypress Warehouses',            capacity = 111,     coords = {x = 1019.116,  y = -2511.69,   z = 28.302 }},
+    [20] = {name = 'West Vinewood Backlot',         capacity = 111,     coords = {x = -245.3405, y = 203.3286,   z = 83.818 }},
+    [21] = {name = 'Old Power Station',             capacity = 42,      coords = {x = 539.346,   y = -1945.682,  z = 24.984 }},
+    [22] = {name = 'Walker & Sons Warehouse',       capacity = 111,     coords = {x = 96.1538,   y = -2216.4,    z = 6.1712 }}
 }
 
 Auto_name = {
-    [1] = 'Налет на федеральное хранилище',
-    [2] = 'Дело "Супердоллары"',
-    [3] = 'Налет на банк',
-    [4] = 'Захват ЭБУ',
-    [5] = 'Налет на тюрьму',
-    [6] = 'Налет на ЦУР',
-    [7] = 'Налет на "Пропащих"',
-    [8] = 'Похищение данных'
+    [1] = 'Union Depository Contract',
+    [2] = 'The Superdollar Deal',
+    [3] = 'The Bank Contract',
+    [4] = 'The E.C.U Job',
+    [5] = 'The Prison Contract',
+    [6] = 'The Agency Deal',
+    [7] = 'The Lost Contract',
+    [8] = 'The Data Contract'
 }
 Auto_val = {
     [1] = 0,
@@ -317,9 +317,9 @@ Auto_val = {
 }
 
 CasApproach_name = {
-    [1] = 'Скрытность',
-    [2] = 'Обман',
-    [3] = 'Штурм'
+    [1] = 'Silent & Sneaky',
+    [2] = 'The Big Con',
+    [3] = 'Aggressive'
 }
 
 CasApproach_val = {
@@ -329,10 +329,10 @@ CasApproach_val = {
 }
 
 CasMain_name = {
-    [1] = 'Деньги',
-    [2] = 'Золото',
-    [3] = 'Картины',
-    [4] = 'Алмазы'
+    [1] = 'Money',
+    [2] = 'Gold',
+    [3] = 'Arts',
+    [4] = 'Diamonds'
 }
 
 CasMain_val = {
@@ -343,35 +343,35 @@ CasMain_val = {
 }
 
 CasGunman_name = {
-    [1] = 'Карл Аболаджи',
-    [2] = 'Густаво Мота',
-    [3] = 'Чарли Рид',
-    [4] = 'Честер Маккой',
-    [5] = 'Патрик Макрири',
-    [6] = 'Стрелок удален'
+    [1] = 'Karl Abolaji',
+    [2] = 'Gustavo Mota',
+    [3] = 'Charlie Reed',
+    [4] = 'Chester McCoy',
+    [5] = 'Patrick McReary',
+    [6] = 'None'
 }
 
 CasDriver_name = {
-    [1] = 'Карим Денц',
-    [2] = 'Талиана Мартинес',
-    [3] = 'Эдди Тоу',
-    [4] = 'Зак Нельсон',
-    [5] = 'Честер Маккой',
-    [6] = 'Водитель удален'
+    [1] = 'Karim Denz',
+    [2] = 'Taliana Martinez',
+    [3] = 'Eddie Toh',
+    [4] = 'Zach Nelson',
+    [5] = 'Chester McCoy',
+    [6] = 'None'
 }
 
 CasHacker_name = {
-    [1] = 'Рикки Люкенс',
-    [2] = 'Кристиан Фелти',
-    [3] = 'Йохан Блэр',
-    [4] = 'Ави Шварцман',
-    [5] = 'Пейдж Харрис',
-    [6] = 'Хакер удален'
+    [1] = 'Rickie Lukens',
+    [2] = 'Christian Feltz',
+    [3] = 'Yohan Blair',
+    [4] = 'Avi Schwartzman',
+    [5] = 'Page Harris',
+    [6] = 'None'
 }
 
 CayoDiff_name = {
-    [1] = 'Высокая сложность',
-    [2] = 'Нормальная сложность'
+    [1] = 'Hard',
+    [2] = 'Normal'
 }
 
 CayoDiff_val = {
@@ -380,11 +380,11 @@ CayoDiff_val = {
 }
 
 CayoMain_name = {
-    [1] = 'Статуя пантеры',
-    [2] = 'Розовый алмаз',
-    [3] = 'Облигации',
-    [4] = 'Ожерелье',
-    [5] = 'Текила "Синсимито"'
+    [1] = 'Statue',
+    [2] = 'Diamond',
+    [3] = 'Bonds',
+    [4] = 'Necklace',
+    [5] = 'Tequila'
 }
 
 CayoMain_val = {
@@ -396,19 +396,19 @@ CayoMain_val = {
 }
 
 CayoSec_name = {
-    [1] = 'Золото',
-    [2] = 'Картины',
-    [3] = 'Кокаин',
-    [4] = 'Марихуана',
-    [5] = 'Деньги'
+    [1] = 'Gold',
+    [2] = 'Arts',
+    [3] = 'Coke',
+    [4] = 'Weed',
+    [5] = 'Cash'
 }
 
 CayoGuns_name = {
-    [1] = 'Зачинщик',
-    [2] = 'Конспиратор',
-    [3] = 'Снайпер',
-    [4] = 'Диверсант',
-    [5] = 'Марксман'
+    [1] = 'Aggressor',
+    [2] = 'Conspirator',
+    [3] = 'Crack shot',
+    [4] = 'Saboteur',
+    [5] = 'Marksman'
 }
 
 CayoGuns_val = {
@@ -453,64 +453,40 @@ Gunvan_coords = {
 }
 
 McSupp_name = {
-    [1] = 'Кокаин',
-    [2] = 'Метамфетамин',
-    [3] = 'Фальшивые деньги',
-    [4] = 'Марихуана',
-    [5] = 'Поддельные документы'
+    [1] = 'Cocaine',
+    [2] = 'Methamphetamine',
+    [3] = 'Counterfeit cash',
+    [4] = 'Weed',
+    [5] = 'Document forgery'
 }
 
 DDAct_name = {
-    [1] = 'Утечка данных',
-    [2] = 'Операция "Богдан"',
-    [3] = 'Сценарий "Судного дня"'
+    [1] = 'The Data Breaches',
+    [2] = 'The Bogdan Problem',
+    [3] = 'The Doomsday Scenario'
 }
 
 Bunkers_name = {
-    [0] = {name = 'Бункер не найден'},
-    [21] = {name = 'Бункер у нефтепромысла',            coords = {x = 492.8395,   y = 3014.057,   z = 39.9793}, type = 5},
-    [22] = {name = 'Бункер в пустыне Гранд-Сенора',     coords = {x = 849.603,    y = 3021.697,   z = 40.3076}, type = 5},
-    [23] = {name = 'Бункер у шоссе 68',                 coords = {x = 39.5967,    y = 2930.506,   z = 54.8034}, type = 5},
-    [24] = {name = 'Бункер на ферме',                   coords = {x = 1572.078,   y = 2226.001,   z = 77.2829}, type = 5},
-    [25] = {name = 'Бункер на Смоук-три-роуд',          coords = {x = 2110.019,   y = 3326.12,    z = 44.3526}, type = 5},
-    [26] = {name = 'Бункер у свалки Томсон',            coords = {x = 2489.36,    y = 3162.12,    z = 48.0015}, type = 5},
-    [27] = {name = 'Бункер в Грейпсид',                 coords = {x = 1801.273,   y = 4705.483,   z = 38.8253}, type = 5},
-    [28] = {name = 'Бункер в лесу Палето',              coords = {x = -755.5687,  y = 5943.835,   z = 18.9008}, type = 5},
-    [29] = {name = 'Бункер в каньоне Ратон',            coords = {x = -388.8392,  y = 4340.109,   z = 55.1741}, type = 5},
-    [30] = {name = 'Бункер в Лаго-Занкудо',             coords = {x = -3031.356,  y = 3334.059,   z = 9.1805 }, type = 5},
-    [31] = {name = 'Бункер в Чумаше',                   coords = {x = -3157.599,  y = 1376.695,   z = 15.866 }, type = 5}
-}
-
-MotoProp_name = {
-    [0] = {name = 'Предприятие не найдено'},
-    [1]  = {name = '[Палето-Бэй] Метамфетамин',                coords = {x = 52.903,     y =  6338.585,  z = 31.35  }, type = 3},
-    [2]  = {name = '[Гора Чилиад] Марихуана',                  coords = {x = 416.7524,   y =  6520.753,  z = 27.7121}, type = 1},
-    [3]  = {name = '[Палето-Бэй] Кокаин',                      coords = {x = 51.7653,    y =  6486.163,  z = 31.428 }, type = 4},
-    [4]  = {name = '[Палето-Бэй] Фальшивые деньги',            coords = {x = -413.6606,  y =  6171.938,  z = 31.4782}, type = 2},
-    [5]  = {name = '[Палето-Бэй] Документы',                   coords = {x = -163.6828,  y =  6334.845,  z = 31.5808}, type = 0},
-    [6]  = {name = '[Эль-Бурро-Хайтс] Метамфетамин',           coords = {x = 1454.671,   y =  -1651.986, z = 67     }, type = 3},
-    [7]  = {name = '[Центр Вайнвуда] Марихуана',               coords = {x = 102.14,     y =  175.26,    z = 104.56 }, type = 1},
-    [8]  = {name = '[Морнингвуд] Кокаин',                      coords = {x = -1462.622,  y =  -381.826,  z = 38.802 }, type = 4},
-    [9]  = {name = '[Канлы Веспуччи] Фальшивые деньги',        coords = {x = -1171.005,  y =  -1380.922, z = 4.937  }, type = 2},
-    [10] = {name = '[Текстайл-Сити] Документы',                coords = {x = 299.071,    y =  -759.072,  z = 29.333 }, type = 0},
-    [11] = {name = '[Пустыня Гранд-Сенора] Метамфетамин',      coords = {x = 201.8909,   y =  2461.782,  z = 55.6885}, type = 3},
-    [12] = {name = '[Сан-Шаньский] Марихуана',                 coords = {x = 2848.369,   y =  4450.147,  z = 48.5139}, type = 1},
-    [13] = {name = '[Аламо-Си] Кокаин',                        coords = {x = 387.5332,   y =  3585.042,  z = 33.2922}, type = 4},
-    [14] = {name = '[Пустыня Гранд-Сенора] Фальшивые деньги',  coords = {x = 636.6344,   y =  2785.126,  z = 42.0111}, type = 2},
-    [15] = {name = '[Грейпсид] Документы',                     coords = {x = 1657.066,   y =  4851.732,  z = 41.9882}, type = 0},
-    [16] = {name = '[Терминал] Метамфетамин',                  coords = {x = 1181.44,    y =  -3113.82,  z = 6.03   }, type = 3},
-    [17] = {name = '[Элизиан-Айленд] Марихуана',               coords = {x = 136.973,    y =  -2472.795, z = 5.98   }, type = 1},
-    [18] = {name = '[Элизиан-Айленд] Кокаин',                  coords = {x = -253.31,    y =  -2591.15,  z = 5.97   }, type = 4},
-    [19] = {name = '[Сайпресс-Флэтс] Фальшивые деньги',        coords = {x = 671.451,    y =  -2667.502, z = 6.0812 }, type = 2},
-    [20] = {name = '[Элизиан-Айленд] Документы',               coords = {x = -331.52,    y =  -2778.97,  z = 5.12   }, type = 0},
+    [0] = {name = 'Bunker not found'},
+    [21] = {name = 'Grand Senora Oilfields Bunker',      coords = {x = 492.8395,   y = 3014.057,   z = 39.9793}, type = 5},
+    [22] = {name = 'Grand Senora Desert Bunker',         coords = {x = 849.603,    y = 3021.697,   z = 40.3076}, type = 5},
+    [23] = {name = 'Route 68 Bunker',                    coords = {x = 39.5967,    y = 2930.506,   z = 54.8034}, type = 5},
+    [24] = {name = 'Farmhouse Bunker',                   coords = {x = 1572.078,   y = 2226.001,   z = 77.2829}, type = 5},
+    [25] = {name = 'Smoke Tree Road Bunker',             coords = {x = 2110.019,   y = 3326.12,    z = 44.3526}, type = 5},
+    [26] = {name = 'Thomson Scrapyard Bunker',           coords = {x = 2489.36,    y = 3162.12,    z = 48.0015}, type = 5},
+    [27] = {name = 'Grapeseed Bunker',                   coords = {x = 1801.273,   y = 4705.483,   z = 38.8253}, type = 5},
+    [28] = {name = 'Paleto Forest Bunker',               coords = {x = -755.5687,  y = 5943.835,   z = 18.9008}, type = 5},
+    [29] = {name = 'Raton Canyon Bunker',                coords = {x = -388.8392,  y = 4340.109,   z = 55.1741}, type = 5},
+    [30] = {name = 'Lago Zancudo Bunker',                coords = {x = -3031.356,  y = 3334.059,   z = 9.1805 }, type = 5},
+    [31] = {name = 'Chumash Bunker',                     coords = {x = -3157.599,  y = 1376.695,   z = 15.866 }, type = 5}
 }
 
 LuckyWheel_name = {
-    'Одежда (1)', 'Одежда (2)', 'Одежда (3)', 'Одежда (4)',
+    'Clothing (1)', 'Clothing (2)', 'Clothing (3)', 'Clothing (4)',
     '2,500 RP', '5,000 RP', '7,500 RP', '10,000 RP', '15,000 RP',
     '$20,000', '$30,000', '$40,000', '$50,000',
-    '10,000 Фишек', '15,000 Фишек', '20,000 Фишек', '25,000 Фишек',
-	'Скидка', 'Тайный приз', 'Машина'
+    '10,000 Chips', '15,000 Chips', '20,000 Chips', '25,000 Chips',
+	'Discount', 'Mystery', 'Vehicle'
 }
 
 LuckyWheel_id = {
@@ -584,13 +560,13 @@ function CasinoCardNameGetter(index)
         card_name = tostring(card_number)
     end
     if index >= 1 and index <= 13 then
-        card_suit = ' (Крести)'
+        card_suit = ' (Club)'
     elseif index >= 14 and index <= 26 then
-        card_suit = ' (Буби)'
+        card_suit = ' (Diamond)'
     elseif index >= 27 and index <= 39 then
-        card_suit = ' (Черви)'
+        card_suit = ' (Heart)'
     elseif index >= 40 and index <= 52 then
-        card_suit = ' (Пики)'
+        card_suit = ' (Spade)'
     end
     return card_name .. card_suit
 end
