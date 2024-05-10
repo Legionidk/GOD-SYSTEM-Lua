e8358621b54ce26a3196bd5b1292f4aaf513c70b
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-05-10 20:01:15 +0300

    v2.6.2

--- godsystem.lua
+++ godsystem.lua
@@ -1,6 +1,6 @@
 --- /godmode --------------------------
 
-LGN = '2.6.1'
+LGN = '2.6.2'
 GTA = '1.68'
 
 --- Инициализация -------------------------------------------------------------
@@ -21,37 +21,6 @@ listener.register('BS_Init', GET_EVENTS_LIST().OnInit, function ()
                 require('godsystem/pages/' .. line .. '/' .. 'init')
             end
         end
-
         Configs.loadSilentConfig()
-        notify.default2('Howdy', 'Howdy, ', social.get_username())
-
-        if fs.directory_exists(fs.get_dir_game()..'\\mods\\update\\x64\\dlcpacks') then
-            addon_checker()
-        else
-            table.insert(Checked_addons, 'folder not found')
-        end
-
-        http.get('https://raw.githubusercontent.com/Legionidk/GOD-SYSTEM-Lua/main/README.md', function (code, header, content)
-            if native.invoke(4, 0xFCA9373EF340AC0A) ~= GTA then
-                notify.warning('notupd', 'Lua is not updated!')
-                console.log(14, '[GOD SYSTEM] Lua is not updated for this game version!\n')
-            else
-                if string.len(LGN) >= 4 then
-                    if string.sub(content, 51, 55) ~= LGN then
-                        notify.warning('newver', 'New version available!')
-                        console.log(14, '[GOD SYSTEM] New version available!\n')
-                    else
-                        log.init('[GOD SYSTEM] Latest version installed')
-                    end
-                else
-                    if string.sub(content, 51, 53) ~= LGN then
-                        notify.warning('newver', 'New version available!')
-                        console.log(14, '[GOD SYSTEM] New version available!\n')
-                    else
-                        log.init('[GOD SYSTEM] Latest version installed')
-                    end
-                end
-            end
-        end)
     end)
 end)
