6bcb0bd386d30c334cc1b3cf2f10104962173697
lgnidk <118756063+Legionidk@users.noreply.github.com>
2024-07-08 10:41:38 +0300

    v3.0

--- godsystem.lua
+++ godsystem.lua
@@ -1,10 +1,10 @@
 -- /godmode // GOD SYSTEM
+require('godsystem/god_vars.lua')
 require('godsystem/god_funcs.lua')
-require('godsystem/god_info.lua')
 Addon_checker()
 
 -- Main page
-Main_page = ui.new_page('GOD SYSTEM v'..Ver, Icons.PORTAL)
+Main_page = ui.new_page('GOD SYSTEM v'..VER, Icons.PORTAL)
 
 -- Pages
 require('godsystem/pages/office.lua')
@@ -18,4 +18,4 @@ require('godsystem/pages/misc.lua')
 require('godsystem/god_events.lua')
 
 -- End
-ui.popup('GOD SYSTEM v'..Ver, 'Loaded successfully.', Icons.PORTAL, PopupType.BOX)
+ui.popup('GOD SYSTEM v'..VER, 'Loaded successfully.', Icons.PORTAL, PopupType.BOX)
