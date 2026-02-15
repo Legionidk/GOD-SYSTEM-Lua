3d779c1ae1902439df61247385771c8653bf2020
lgnidk <legionidk@internet.ru>
2026-02-15 16:09:00 +0300

    updated bunke info group

--- godSystem/pages/businesses/bunker.lua
+++ godSystem/pages/businesses/bunker.lua
@@ -1,29 +1,44 @@
 local bunkerSubpage = BUSINESSES_PAGE:new_subpage("Bunker")
 
-local infoGroup = bunkerSubpage:new_group("Stock, supplies and research info", 1)
+local infoGroup = bunkerSubpage:new_group("Stock, research and supplies info", 1)
 local stockPriceGroup = bunkerSubpage:new_group("Stock price", 1)
 local stockProductionGroup = bunkerSubpage:new_group("Stock production", 2)
 local stockSellGroup = bunkerSubpage:new_group("Sell stock", 2)
 local suppliesGroup = bunkerSubpage:new_group("Supplies", 3)
 
+-- region data
+local bunkersData = {
+    [0]  = {name = "Bunker not found"},
+    [21] = {name = "Grand Senora Oilfields Bunker", coords = {x = 492.8395,  y = 3014.057, z = 39.9793}},
+    [22] = {name = "Grand Senora Desert Bunker",    coords = {x = 849.603,   y = 3021.697, z = 40.3076}},
+    [23] = {name = "Route 68 Bunker",               coords = {x = 39.5967,   y = 2930.506, z = 54.8034}},
+    [24] = {name = "Farmhouse Bunker",              coords = {x = 1572.078,  y = 2226.001, z = 77.2829}},
+    [25] = {name = "Smoke Tree Road Bunker",        coords = {x = 2110.019,  y = 3326.12,  z = 44.3526}},
+    [26] = {name = "Thomson Scrapyard Bunker",      coords = {x = 2489.36,   y = 3162.12,  z = 48.0015}},
+    [27] = {name = "Grapeseed Bunker",              coords = {x = 1801.273,  y = 4705.483, z = 38.8253}},
+    [28] = {name = "Paleto Forest Bunker",          coords = {x = -755.5687, y = 5943.835, z = 18.9008}},
+    [29] = {name = "Raton Canyon Bunker",           coords = {x = -388.8392, y = 4340.109, z = 55.1741}},
+    [30] = {name = "Lago Zancudo Bunker",           coords = {x = -3031.356, y = 3334.059, z = 9.1805 }},
+    [31] = {name = "Chumash Bunker",                coords = {x = -3157.599, y = 1376.695, z = 15.866 }}
+}
+-- endregion
+
 -- region infoGroup
-local stockInfoSlider = infoGroup:new_slider("Stock", 0, 100):set_enabled(false)
-local researchInfoSlider = infoGroup:new_slider("Research", 0, 100):set_enabled(false)
-local suppliesInfoSlider = infoGroup:new_slider("Supplies", 0, 100):set_enabled(false)
+STOCK_INFO_SLIDER = infoGroup:new_slider("Stock", 0, 100):set_enabled(false)
+RESEARCH_INFO_SLIDER = infoGroup:new_slider("Research", 0, 100):set_enabled(false)
+SUPPLIES_INFO_SLIDER = infoGroup:new_slider("Supplies", 0, 100):set_enabled(false)
 
-function UPDATE_BUNKER_INFO_SLIDERS()
-    local bunkerStockHash = string.smart_joaat("MP" .. stats.get_character_index() .. "_PRODTOTALFORFACTORY5")
-    local bunkerResearchHash = string.smart_joaat("MP" .. stats.get_character_index() .. "_RESEARCHTOTALFORFACTORY5")
-    local bunkerSuppliesHash = string.smart_joaat("MP" .. stats.get_character_index() .. "_MATTOTALFORFACTORY5")
+infoGroup:new_button("Teleport to your bunker", function ()
+    local bunkerHash = string.smart_joaat('MP'..stats.get_character_index()..'_FACTORYSLOT5')
+    local bunkerData = bunkersData[stats.get_u32(bunkerHash)]
 
-    stockInfoSlider:set(stats.get_u32(bunkerStockHash))
-    researchInfoSlider:set(stats.get_u32(bunkerResearchHash))
-    suppliesInfoSlider:set(stats.get_u32(bunkerSuppliesHash))
-end
+    if bunkerData == 0 then
+        ui.popup("GOD SYSTEM", "Bunker is not found.", Icons.CANCEL2, PopupType.BOX)
+        return
+    end
 
-infoGroup:new_button("Update", function ()
-    UPDATE_BUNKER_INFO_SLIDERS()
-    ui.popup("GOD SYSTEM", "Bunker info group has been updated.",Icons.CHECKMARK_SUCCESS, PopupType.BOX)
+    utils.teleport_self(bunkerData.coords.x, bunkerData.coords.y, bunkerData.coords.z)
+    ui.popup("GOD SYSTEM", "Successfully teleported to "..bunkerData.name..".", Icons.CHECKMARK_SUCCESS, PopupType.BOX)
 end)
 -- endregion
 
