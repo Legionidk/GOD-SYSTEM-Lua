ffd509b1439cdad83f086cd0801435bbc60f7d72
lgnidk <legionidk@internet.ru>
2026-02-15 03:47:31 +0300

    updated casino page

--- godSystem/pages/casino/_init.lua
+++ godSystem/pages/casino/_init.lua
@@ -1,9 +1,11 @@
-local casinoPage = ui.new_page("[GS] Casino", Icons.PORTAL)
+local casinoPage = ui.new_page("/GS/ Casino", Icons.GAMEPAD)
 local casinoSubpage = casinoPage:new_subpage("Casino")
 
-local luckyWheelGroup = casinoSubpage:new_group("Lucky wheel", 1)
-local slotsGroup = casinoSubpage:new_group("Slots", 2)
+local chipsGroup = casinoPage:new_group("Chips", 1)
+local slotsGroup = casinoSubpage:new_group("Slots", 1)
+local luckyWheelGroup = casinoSubpage:new_group("Lucky wheel", 2)
 local cooldownsGroup = casinoSubpage:new_group("Cooldowns", 3)
+local rouletteGroup = casinoSubpage:new_group("Roulette", 3):set_visible(false)
 
 -- region data
 local luckyWheelDropData = {
@@ -33,23 +35,26 @@ local cooldownsData = {
 }
 -- endregion
 
--- region luckyWheelGroup
-luckyWheelGroup:new_text("Click on item in list below while you're on a casino to instant drop from luckywheel.")
+-- region chipsGroup
+local limitNumberSlider = chipsGroup:new_slider("Limit number", 20000, 2147483647)
 
-local luckyWheelDropTable = luckyWheelGroup:new_table_list("Lucky wheel drop", luckyWheelDropData.names)
-
-luckyWheelDropTable:set_click_callback(function (index)
-    if not script.is_running("casino_lucky_wheel") then
-        ui.popup("GOD SYSTEM", "You're not in the casino.", Icons.CANCEL2, PopupType.BOX)
-        return
-    end
-
-    script.get_local("casino_lucky_wheel", 304 + 14):write_u32(luckyWheelDropData.indexes[index])
-    script.get_local("casino_lucky_wheel", 304 + 45):write_u32(11)
-    ui.popup("GOD SYSTEM", "Drop from lucky wheel has been successfully given to you: "..luckyWheelDropData.names[index]..".", Icons.CHECKMARK_SUCCESS, PopupType.BOX)
+chipsGroup:new_button("Set new acquire limit", function ()
+    tunables.set_int("VC_CASINO_CHIP_MAX_BUY", limitNumberSlider:get())
+    tunables.set_int("VC_CASINO_CHIP_MAX_BUY_PENTHOUSE", limitNumberSlider:get())
+    ui.popup("GOD SYSTEM", "Acquire limit has been changed.", Icons.CHECKMARK_SUCCESS, PopupType.BOX)
 end)
 
-luckyWheelDropTable:set_hint("test")
+chipsGroup:new_button("Set new trade in limit", function ()
+    tunables.set_int("VC_CASINO_CHIP_MAX_SELL", limitNumberSlider:get())
+    ui.popup("GOD SYSTEM", "Trade in limit has been changed.", Icons.CHECKMARK_SUCCESS, PopupType.BOX)
+end)
+
+chipsGroup:new_button("Reset new all limits", function ()
+    tunables.set_int("VC_CASINO_CHIP_MAX_BUY", 20000)
+    tunables.set_int("VC_CASINO_CHIP_MAX_BUY_PENTHOUSE", 50000)
+    tunables.set_int("VC_CASINO_CHIP_MAX_SELL", 10000000)
+    ui.popup("GOD SYSTEM", "All limits has been changed.", Icons.CHECKMARK_SUCCESS, PopupType.BOX)
+end)
 -- endregion
 
 -- region slotsGroup
@@ -82,6 +87,25 @@ slotsGroup:new_button("Lose slots", function ()
 end)
 -- endregion
 
+-- region luckyWheelGroup
+luckyWheelGroup:new_text("Click on item in list below while you're on a casino to instant drop from lucky wheel.")
+
+local luckyWheelDropTable = luckyWheelGroup:new_table_list("Lucky wheel drop", luckyWheelDropData.names)
+
+luckyWheelDropTable:set_click_callback(function (index)
+    if not script.is_running("casino_lucky_wheel") then
+        ui.popup("GOD SYSTEM", "You're not in the casino.", Icons.CANCEL2, PopupType.BOX)
+        return
+    end
+
+    script.get_local("casino_lucky_wheel", 304 + 14):write_u32(luckyWheelDropData.indexes[index])
+    script.get_local("casino_lucky_wheel", 304 + 45):write_u32(11)
+    ui.popup("GOD SYSTEM", "Drop from lucky wheel has been successfully given to you: "..luckyWheelDropData.names[index]..".", Icons.CHECKMARK_SUCCESS, PopupType.BOX)
+end)
+
+luckyWheelDropTable:set_hint("test")
+-- endregion
+
 -- region cooldownsGroup
 cooldownsGroup:new_button("Delete all cooldowns", function ()
     for _, v in pairs(cooldownsData) do
@@ -90,6 +114,18 @@ cooldownsGroup:new_button("Delete all cooldowns", function ()
     end
 
     ui.popup("GOD SYSTEM", "All cooldowns has been deleted.", Icons.CHECKMARK_SUCCESS, PopupType.BOX)
+end)
+-- endregion
+
+-- region rouletteGroup
+rouletteGroup:new_button("Land on black 13", function ()
+    script.force_host("casinoroulette", function ()
+        for i = 0, 5 do
+            script.get_local("casinoroulette", 148 + 1357 + 153 + i):write_u32(13)
+            ui.popup("GOD SYSTEM", "Ball will now land on black 13.", Icons.CHECKMARK_SUCCESS, PopupType.BOX)
+            return
+        end
+    end)
 end)
 -- endregion
 
